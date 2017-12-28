e2404a41c1da52f0bce9d66321e0a27b8fc4c210
ElSithValandil <cherrera.marin@gmail.com>
2017-12-28 11:30:23 -0300

    Initial commit

---
+++ MyFoto/app/components/SimpleButton.js
@@ -0,0 +1,17 @@
+import	React,	{
+    Text,
+    Component,
+    View
+} from 'react-native';
+
+class SimpleButton extends Component{
+    render	()	{
+        return	(
+            <View>
+                <Text>Botón</Text>
+            </View>
+        );
+    }
+}
+
+module.exports = SimpleButton;
